de86e7bba6867e374d75571f98445793fe59fcbe
NA-Siddiky <siddiky.academic@gmail.com>
2023-04-29 01:31:33 +0600

    set click point

--- eye.py
+++ eye.py
@@ -11,6 +11,8 @@ while True:
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     output = face_mesh.process(rgb_frame)
     landmark_points = output.multi_face_landmarks
+#    print(landmark_points)
+
     frame_h, frame_w, _ = frame.shape
     if landmark_points:
         landmarks = landmark_points[0].landmark
@@ -18,16 +20,24 @@ while True:
             x = int(landmark.x * frame_w)
             y = int(landmark.y * frame_h)
             cv2.circle(frame, (x, y), 3, (0, 255, 0))
+
+            #control mouse according to screen ratio
             if id == 1:
-                screen_x = screen_w * landmark.x
-                screen_y = screen_h * landmark.y
+                screen_x = 1.25*screen_w * landmark.x
+                screen_y = 1.25*screen_h * landmark.y
                 pyautogui.moveTo(screen_x, screen_y)
+
+        #handle click
         left = [landmarks[145], landmarks[159]]
         for landmark in left:
             x = int(landmark.x * frame_w)
             y = int(landmark.y * frame_h)
             cv2.circle(frame, (x, y), 3, (0, 255, 255))
-        if (left[0].y - left[1].y) < 0.004:
+        
+        #measure value for operation click
+        #print(left[0].y - left[1].y)
+        if (left[0].y - left[1].y) < 0.007:
+            print("click>>")
             pyautogui.click()
             pyautogui.sleep(1)
     cv2.imshow('Eye Controlled Mouse', frame)
